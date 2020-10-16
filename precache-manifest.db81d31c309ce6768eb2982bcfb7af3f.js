8011c88c1ed273695930fa26d585a34e0127d9fd
imashu001 <imashu001@gmail.com>
2020-10-16 11:42:25 +0530

    Updates

---
+++ precache-manifest.db81d31c309ce6768eb2982bcfb7af3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8531c2f19619b826f33c5526df5a6e91",
+    "url": "/github.io/rweather/index.html"
+  },
+  {
+    "revision": "f2b601ff0a5330c21d56",
+    "url": "/github.io/rweather/static/css/main.ef335960.chunk.css"
+  },
+  {
+    "revision": "03b373eba737944ded78",
+    "url": "/github.io/rweather/static/js/2.3af2a60c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github.io/rweather/static/js/2.3af2a60c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2b601ff0a5330c21d56",
+    "url": "/github.io/rweather/static/js/main.c7aacfb8.chunk.js"
+  },
+  {
+    "revision": "ba03a6fbbc0eaeedf8c6",
+    "url": "/github.io/rweather/static/js/runtime-main.da389a6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github.io/rweather/static/media/logo.5d5d9eef.svg"
+  }
+]);
